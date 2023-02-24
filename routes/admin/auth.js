ad89cc495154484b6bf41dbdadcacdf911eea214
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-02-24 15:52:52 +0000

    Created validators for sign in, updated auth, updated error in sign in

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -4,7 +4,13 @@ const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
-const { requireEmail, requirePassword, requirePasswordConfirmation } = require('./validators')
+const { 
+    requireEmail, 
+    requirePassword,
+    requirePasswordConfirmation,
+    requireEmailExists,
+    requireValidPassword
+} = require('./validators')
 
 const router = express.Router();
 
@@ -21,7 +27,6 @@ router.post('/signup',
         }
 
         const { email, password, passwordConfirmation } = req.body;
-        
         const user = await usersRepo.create({ email, password });
         req.session.userId = user.id;
         
@@ -34,26 +39,24 @@ router.get('/signout', (req, res) => {
 });
 
 router.get('/signin', (req, res) => {
-    res.send(signinTemplate());
+    res.send(signinTemplate({}));
 });
 
-router.post('/signin', async (req, res) => {
-    const { email, password } = req.body;
-    const user = await usersRepo.getOneBy({ email });
-    if (!user) {
-        return res.send('Email not found');
-    }
-    const validPassword = await usersRepo.comparePasswords(
-        user.password,
-        password
-    )
-    if (!validPassword) {
-        return res.send('Invalid password');
-    }
+router.post('/signin',
+    [requireEmailExists, requireValidPassword],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.send(signinTemplate({ errors }));
+        }
 
-    req.session.userId = user.id;
-    
-    res.send('Signed in');
+        const { email } = req.body;
+
+        const user = await usersRepo.getOneBy({ email });
+
+        req.session.userId = user.id;
+        
+        res.send('Signed in');
 });
 
 module.exports = router;
