87c0b5e8da124ea1b6152a8d8b4317a008438849
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-02-24 14:08:30 +0000

    Created signin and signup, updated auth, structure refactor

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const usersRepo = require('../../repositories/users');
+const signupTemplate = require('../../views/admin/auth/signup');
+const signinTemplate = require('../../views/admin/auth/signin');
 
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
-    res.send(`
-        <div>
-            Your id is: ${req.session.userId}
-            <form method="POST">
-                <input name="email" type="email" placeholder="email" />
-                <input name="password" type="password" placeholder="password" />
-                <input name="passwordConfirmation" type="password" placeholder="passwordConfirmation" />
-                <button>Sign Up</button>
-            </form>
-        </div>
-    `);
+    res.send(signupTemplate({ req }));
 });
 
 router.post('/signup', async (req, res) => {
@@ -39,15 +31,7 @@ router.get('/signout', (req, res) => {
 });
 
 router.get('/signin', (req, res) => {
-    res.send(`
-        <div>
-            <form method="POST">
-                <input name="email" type="email" placeholder="email" />
-                <input name="password" type="password" placeholder="password" />
-                <button>Sign In</button>
-            </form>
-        </div>
-    `);
+    res.send(signinTemplate());
 });
 
 router.post('/signin', async (req, res) => {
