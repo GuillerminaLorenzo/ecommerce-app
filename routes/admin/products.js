6c3b6284eb8e11b5a830b18896ea417c5b6e3935
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-02-28 14:54:35 +0000

    Added validators for newProduct input

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const productsRepo = require('../../repositories/products');
 const newProductTemplate = require('../../views/admin/products/newProduct');
+const { requireTitle, requirePrice } = require('./validators');
+const { validatorResult } = require('express-validator');
 
 const router = express.Router();
 
@@ -8,6 +10,11 @@ router.get('/admin/products', (req, res) => {});
 
 router.get('/admin/products/new', (req, res) => {
     res.send(newProductTemplate({}));
+});
+
+router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
+    const errors = validationResult(req);
+    res.send('Submitted');
 });
 
 module.exports = router;
--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -2,6 +2,15 @@ const { check } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 
 module.exports = {
+    requireTitle: check('title')
+        .trim()
+        .isLength({ min:5, max:40 })
+        .withMessage('no'),
+    requirePrice: check('price')
+        .trim()
+        .toFloat()
+        .isFloat({ min:1 })
+        .withMessage('no'),
     requireEmail:  check('email')
         .trim()
         .normalizeEmail()
