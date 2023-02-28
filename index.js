a5de7ef085eab93aa79fddccf7d1e87727adfe6a
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-02-28 12:04:52 +0000

    Created products, its paths and added it to index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,7 @@ app.use(cookieSession({ keys: ['kcnsjbdv123'] }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(authRouter);
+app.use(productsRouter);
   
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
