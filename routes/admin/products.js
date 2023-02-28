83d7b77f20a3d3785165bce4a545810f2e086c37
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-02-28 15:13:31 +0000

    Updated newProduc and product to get info about the image using multer

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const { validatorResult } = require('express-validator');
+const multer = require('multer');
+
 const productsRepo = require('../../repositories/products');
 const newProductTemplate = require('../../views/admin/products/newProduct');
 const { requireTitle, requirePrice } = require('./validators');
-const { validatorResult } = require('express-validator');
+
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() })
 
 router.get('/admin/products', (req, res) => {});
 
@@ -12,9 +16,13 @@ router.get('/admin/products/new', (req, res) => {
     res.send(newProductTemplate({}));
 });
 
-router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
-    const errors = validationResult(req);
-    res.send('Submitted');
-});
+router.post('/admin/products/new', 
+    [requireTitle, requirePrice], 
+    upload.single('image'), 
+    (req, res) => {
+        const errors = validationResult(req);
+        res.send('Submitted');
+    }
+);
 
 module.exports = router;
