365117306e1de2320725da519440a2f0cf78649e
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-03-01 15:36:07 +0000

    Created requireAuth middleware

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -5,22 +5,23 @@ const productsRepo = require('../../repositories/products');
 const newProductTemplate = require('../../views/admin/products/new');
 const indexProductTemplate = require('../../views/admin/products/index');
 const { requireTitle, requirePrice } = require('./validators');
-const { handleErrors } = require('./middlewares');
+const { handleErrors, requireAuth } = require('./middlewares');
 
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() })
 
-router.get('/admin/products', async (req, res) => {
+router.get('/admin/products', requireAuth, async (req, res) => {
     const products = await productsRepo.getAll();
     res.send(indexProductTemplate({ products }));
 });
 
-router.get('/admin/products/new', (req, res) => {
+router.get('/admin/products/new', requireAuth, (req, res) => {
     res.send(newProductTemplate({}));
 });
 
 router.post('/admin/products/new', 
+    requireAuth,
     upload.single('image'),
     [requireTitle, requirePrice], 
     handleErrors(newProductTemplate),
