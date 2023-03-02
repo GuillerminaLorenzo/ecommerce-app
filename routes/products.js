707b1f2c7962b08b10ab2814a71dbc6ce42d3b10
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-03-02 11:24:39 +0000

    Created and added view template for GET/ indexProducts

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const productsRepo = require('../repositories/products');
+const indexProductsTemplate = require('../views/products/index');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {});
+router.get('/', async (req, res) => {
+    const products = await productsRepo.getAll();
+    res.send(indexProductsTemplate({ products }));
+});
 
 module.exports = router;
