d98786bf94d7812d40f9a69a9656ee6f3e4cf157
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-03-03 15:33:35 +0000

    Homepage test 'has products in nav bar'

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -6,4 +6,13 @@ describe('Home page', () => {
   it('has a title', () => {
     cy.get('h3[id=homepage-title]').should('contain', 'EComm');
   });
+
+  it('has products in nav bar', () => {
+    cy.get('a[id=homepage-products]')
+      .should('contain', 'Products')
+      .click();
+
+    cy.url().should("include", "/");
+    cy.contains('h3[id=homepage-title]', 'EComm');
+  });
 })
