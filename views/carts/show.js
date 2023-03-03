a6a95f38659451c6f5c976096ab03522f10ff4af
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-03-03 13:55:00 +0000

    Updated styling view show cart

--- views/carts/show.js
+++ views/carts/show.js
@@ -1,8 +1,10 @@
 const layout = require('../layout');
 
 module.exports = ({ items }) => {
-  const renderedItems = items
-    .map(item => {
+    const totalPrice = items.reduce((prev, item) => {
+        return prev + item.quantity * item.product.price;
+    }, 0);
+    const renderedItems = items.map(item => {
       return `
         <div class="cart-item message">
           <h3 class="subtitle">${item.product.title}</h3>
@@ -14,7 +16,8 @@ module.exports = ({ items }) => {
               $${item.product.price * item.quantity}
             </div>
             <div class="remove">
-              <form method="POST">
+              <form method="POST" action="/cart/products/delete">
+                <input hidden value="${item.id}" name="itemId" />
                 <button class="button is-danger">                  
                   <span class="icon is-small">
                     <i class="fas fa-times"></i>
@@ -25,30 +28,29 @@ module.exports = ({ items }) => {
           </div>
         </div>
       `;
-    })
-    .join('');
+    }).join('');
 
-  return layout({
-    content: `
-      <div id="cart" class="container">
-        <div class="columns">
-          <div class="column"></div>
-          <div class="column is-four-fifths">
-            <h3 class="subtitle"><b>Shopping Cart</b></h3>
-            <div>
-              ${renderedItems}
+    return layout({
+        content: `
+        <div id="cart" class="container">
+            <div class="columns">
+            <div class="column"></div>
+            <div class="column is-four-fifths">
+                <h3 class="subtitle"><b>Shopping Cart</b></h3>
+                <div>
+                ${renderedItems}
+                </div>
+                <div class="total message is-info">
+                <div class="message-header">
+                    Total
+                </div>
+                <h1 class="title">$${totalPrice}</h1>
+                <button class="button is-primary">Buy</button>
+                </div>
             </div>
-            <div class="total message is-info">
-              <div class="message-header">
-                Total
-              </div>
-              <h1 class="title">$</h1>
-              <button class="button is-primary">Buy</button>
+            <div class="column"></div>
             </div>
-          </div>
-          <div class="column"></div>
         </div>
-      </div>
-    `
-  });
+        `
+    });
 };
