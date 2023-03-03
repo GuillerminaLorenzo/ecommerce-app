e83b39180c5d690135a1d43cafa69489bd3c8f61
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-03-03 14:42:46 +0000

    Installed cypress

---
+++ cypress/e2e/homepage.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  });
+  
+  describe('My First Test', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(true)
+    })
+  })
+})
