e44effafc2e7cc72f6d32160b98b55184ea60076
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-03-03 11:27:33 +0000

    Created GET/cart

--- routes/carts.js
+++ routes/carts.js
@@ -27,5 +27,19 @@ router.post('/cart/products', async (req, res) => {
     res.send('Product added');
 });
 
+router.get('/cart', async (req, res) => {
+    if(!req.session.cartId) {
+        return res.redirect('/');
+    }
+
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    for(let item of cart.items) {
+        const product = await productsRepo.getOne(item.id);
+        item.product = product;
+    }
+
+    res.send(cartShowTemplate({ items: cart.items }));
+});
 
 module.exports = router;
