18a5ed8e11a2c89f532a9ea9f056551cddcf260b
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-03-03 11:26:46 +0000

    Created POST/cart/products

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,31 @@
 const express = require('express');
+const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
+
+router.post('/cart/products', async (req, res) => {
+    let cart;
+    if(!req.session.cartId) {
+        cart = await cartsRepo.create({ items: [] });
+        req.session.cartId = cart.id;
+    } else {
+        cart = await cartsRepo.getOne(req.session.cartId);
+    }
+
+    const existingItem = cart.items.find(item => item.id === req.body.productId);
+    if(existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.items.push({ id: req.body.productId, quantity: 1 });
+    }
+    await cartsRepo.update(cart.id, {
+        items: cart.items
+    });
+
+    res.send('Product added');
+});
+
 
 module.exports = router;
